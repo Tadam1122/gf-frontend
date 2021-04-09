41f8820687d4b117fbfc6b9734deb30c85648558
Taylor <epiphonetaylor@gmail.com>
2021-04-09 13:37:18 -0600

    redirect out of wishlist page if not logged in

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Route, Redirect } from 'react-router-dom'
 import {
   Container,
   Grow,
@@ -67,7 +67,11 @@ function Wishlists({ location }) {
   const products = useSelector((state) => state.wishlistRed.products)
   const dispatch = useDispatch()
 
-  const wishlist = location.state.wishlist
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  let wishlist = {}
+  if (location.state) {
+    wishlist = location.state.wishlist
+  }
 
   // ui state variables
   const [order, setOrder] = useState('asc')
@@ -78,10 +82,12 @@ function Wishlists({ location }) {
 
   useEffect(() => {
     async function getProducts() {
-      dispatch(fetchWishlistProducts(wishlist.items))
+      if (location.state) {
+        dispatch(fetchWishlistProducts(wishlist.items))
+      }
     }
     getProducts()
-  }, [wishlist, dispatch])
+  }, [wishlist, location, dispatch])
 
   // get new header cells for different categories
   useEffect(() => {
@@ -120,6 +126,7 @@ function Wishlists({ location }) {
 
   function handleDeleteItem(id) {
     let found = false
+    // eslint-disable-next-line array-callback-return
     wishlist.items = wishlist.items.filter((product) => {
       if (product.id === id && !found) {
         found = true
@@ -131,6 +138,7 @@ function Wishlists({ location }) {
     //get updated wishlist price
     let updatedProducts = [...products]
     found = false
+    // eslint-disable-next-line array-callback-return
     updatedProducts = updatedProducts.filter((product) => {
       if (product._id === id && !found) {
         found = true
@@ -168,69 +176,80 @@ function Wishlists({ location }) {
   }
 
   return (
-    <Container className={classes.root}>
-      <Grow in={true}>
-        <div>
-          <TableContainer className={classes.tableContainer}>
-            <Toolbar className={classes.toolbar}>
-              <Typography component='div' variant='h3'>
-                {wishlist.name}
-              </Typography>
-            </Toolbar>
-            <Table stickyHeader>
-              <ItemTableHead
-                order={order}
-                orderBy={orderBy}
-                handleSort={handleSort}
-                headerCells={headerCells}
-                handleModalOpen={handleModalOpen}
-              />
-              <TableBody>
-                {products.length > 0 &&
-                  sortData(products, order, orderBy).map((product) => (
-                    <ItemTableRow
-                      product={product}
-                      key={product.model}
+    <Route
+      render={() =>
+        user ? (
+          <Container className={classes.root}>
+            <Grow in={true}>
+              <div>
+                <TableContainer className={classes.tableContainer}>
+                  <Toolbar className={classes.toolbar}>
+                    <Typography component='div' variant='h3'>
+                      {wishlist.name}
+                    </Typography>
+                  </Toolbar>
+                  <Table stickyHeader>
+                    <ItemTableHead
+                      order={order}
+                      orderBy={orderBy}
+                      handleSort={handleSort}
                       headerCells={headerCells}
-                      handleDeleteItem={handleDeleteItem}
+                      handleModalOpen={handleModalOpen}
                     />
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Grid
-            container
-            align='right'
-            direction='column'
-            className={classes.footerContainer}
-          >
-            <Grid item>
-              <Typography className={classes.footer} variant='h4'>
-                {wishlist.totalPrice}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Button
-                className={classes.footer}
-                variant='contained'
-                color='secondary'
-                onClick={handleDelModalOpen}
-                disableElevation
-              >
-                Delete Wishlist
-              </Button>
-            </Grid>
-          </Grid>
-          <BrowseModal modalOpen={modalOpen} handleClose={handleModalClose} />
-          <DeleteModal
-            modalOpen={delModalOpen}
-            handleClose={handleDelModalClose}
-            wishlist={wishlist}
-            handleDeleteWishlist={handleDeleteWishlist}
-          />
-        </div>
-      </Grow>
-    </Container>
+                    <TableBody>
+                      {products.length > 0 &&
+                        sortData(products, order, orderBy).map((product) => (
+                          <ItemTableRow
+                            product={product}
+                            key={product.model}
+                            headerCells={headerCells}
+                            handleDeleteItem={handleDeleteItem}
+                          />
+                        ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <Grid
+                  container
+                  align='right'
+                  direction='column'
+                  className={classes.footerContainer}
+                >
+                  <Grid item>
+                    <Typography className={classes.footer} variant='h4'>
+                      {wishlist.totalPrice}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      className={classes.footer}
+                      variant='contained'
+                      color='secondary'
+                      onClick={handleDelModalOpen}
+                      disableElevation
+                    >
+                      Delete Wishlist
+                    </Button>
+                  </Grid>
+                </Grid>
+                <BrowseModal
+                  modalOpen={modalOpen}
+                  handleClose={handleModalClose}
+                />
+                <DeleteModal
+                  modalOpen={delModalOpen}
+                  handleClose={handleDelModalClose}
+                  wishlist={wishlist}
+                  handleDeleteWishlist={handleDeleteWishlist}
+                />
+              </div>
+            </Grow>
+          </Container>
+        ) : (
+          <Redirect to={{ pathname: '/' }} />
+        )
+      }
+    />
   )
 }
 
