418da23c039f43a4e7c4d4821e1b98c0524d5d6b
Taylor <epiphonetaylor@gmail.com>
2021-04-09 19:34:12 -0600

    removed redux browser extension setup

--- src/store.js
+++ src/store.js
@@ -19,10 +19,7 @@ const middleware = [thunk]
 const store = createStore(
   persistedReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 )
 export const persistor = persistStore(store)
 
