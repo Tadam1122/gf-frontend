ab7a0118bb5c7a26679763de7a7b32cf8302bce9
Taylor <epiphonetaylor@gmail.com>
2021-04-15 18:28:31 -0600

    api url debugged

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
 
-const url =
-  process.env.MONGO_USER && process.env.MONGO_PASS
-    ? '/api'
-    : 'http://localhost:8000/api'
-
 //TODO: swap baseURL to env var in production '/api'
 export function http() {
   return axios.create({
-    baseURL: url,
+    baseURL: '/api',
     headers: {
       authorization: localStorage.getItem('token'),
     },
